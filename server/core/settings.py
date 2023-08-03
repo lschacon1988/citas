a46823af85e7284bbbd6e7a84e987164ca59f638
lschacon1988 <lschacon1988@gmail.com>
2023-08-03 12:44:34 -0500

    update setting

--- server/core/settings.py
+++ server/core/settings.py
@@ -158,20 +158,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-if 'RAILWAY_STATIC_URL' in os.environ:
-    STATIC_URL = os.environ.get('RAILWAY_STATIC_URL', '/static/')
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 STATIC_URL = '/static/'
-STATICFILE_DIRS = [
-    BASE_DIR / 'static',
-    BASE_DIR / 'staticfiles',
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- server/core/urls.py
+++ server/core/urls.py
@@ -39,6 +39,8 @@ urlpatterns = [
     path('accounts/profile/', Profile.as_view(), name='profile'),
 ]
 
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
